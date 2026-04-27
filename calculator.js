5e8ca2f9c0879a1e4e92ac7e951476640b4728f2
B-Serhii0 <ser090108@gmail.com>
2026-04-27 14:21:48 +0300

    Implemented feature XI

--- calculator.js
+++ calculator.js
@@ -1,3 +1,23 @@
 'use strict'
 
-console.log("sanya qwe")
+let number1 = +prompt("n1:");
+if (isNaN(number1)) {
+    alert("We must write a number, not a string");
+}
+
+let number2 = +prompt("n2:");
+if (isNaN(number2)) {
+    alert("We must write a number, not a string");
+}
+
+let result = 0;
+
+/*=========================================================================*/
+
+let sign = prompt("/,+,-,* :")
+
+function calc() {
+    alert("Sergey");
+}
+
+calc();
