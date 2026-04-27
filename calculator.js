1835b05fd9b8c83dcbe47444eddd3e530d08f393
kladetka <sashasmile178@gmail.com>
2026-04-27 14:17:24 +0300

    updeta

--- calculator.js
+++ calculator.js
@@ -2,35 +2,22 @@
 
 let number1 = +prompt("n1:");
 if (isNaN(number1)) {
-    return alert("We must write a number, not a string");
-}
-let number2 = +prompt("n2:");
-if (isNaN(number2)) {
-    return alert("We must write a number, not a string");
+    alert("We must write a number, not a string");
 }
 
+let number2 = +prompt("n2:");
+if (isNaN(number2)) {
+    alert("We must write a number, not a string");
+}
 
 let result = 0;
 
 /*=========================================================================*/
 
 let sign = prompt("/,+,-,* :")
-calc()
+
 function calc() {
-    switch (sign) {
-        case "+":
-            result = number1 + number2;
-            return alert(`Result:${result}`)
-        case "/":
-            result = number1 / number2;
-            return alert(`Result:${result}`)
-        case "-":
-            result = number1 - number2;
-            return alert(`Result:${result}`)
-        case "*":
-            result = number1 * number2;
-            return alert(`Result:${result}`)
-    }
+    alert("Sergey");
 }
 
-
+calc();
