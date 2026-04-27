dfe61c977d933fe2dc20ead3f99096e2869874bf
B-Serhii0 <ser090108@gmail.com>
2026-04-27 14:14:29 +0300

    Implemented feature X

--- calculator.js
+++ calculator.js
@@ -1,36 +1,3 @@
 'use strict'
 
-let number1 = +prompt("n1:");
-if (isNaN(number1)) {
-    return alert("We must write a number, not a string");
-}
-let number2 = +prompt("n2:");
-if (isNaN(number2)) {
-    return alert("We must write a number, not a string");
-}
-
-
-let result = 0;
-
-/*=========================================================================*/
-
-let sign = prompt("/,+,-,* :")
-calc()
-function calc() {
-    switch (sign) {
-        case "+":
-            result = number1 + number2;
-            return alert(`Result:${result}`)
-        case "/":
-            result = number1 / number2;
-            return alert(`Result:${result}`)
-        case "-":
-            result = number1 - number2;
-            return alert(`Result:${result}`)
-        case "*":
-            result = number1 * number2;
-            return alert(`Result:${result}`)
-    }
-}
-
-
+console.log("sanya qwe")
